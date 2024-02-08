0587a0453e4a67c96db9d4d4d367ba502485b327
Jessica Colmenares <104736248+jessijane22@users.noreply.github.com>
2024-02-08 19:31:01 +0000

    ejemplos de variables

---
+++ henry/variables.js
@@ -0,0 +1,24 @@
+//string
+var bootcamp='Henry';
+
+//Numeros
+var numerosEnteros=17;
+var numerosNegativos=-4;
+var numeroDecimal=11.75;
+
+//Booleanos
+var estoyEntendiendo=true;
+var estoyAburrido=false;
+
+// undefined
+var cajon1;
+console.log(cajon1);
+
+// Undefined
+var cajon1;
+console.log(cajon1);
+
+//Null
+var sinNada = null;
+
+
